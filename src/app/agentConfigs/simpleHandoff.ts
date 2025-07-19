c5ce161f5f370a291452496ba444fa088719b2df
Ómar Örn <omarorn@gmail.com>
2025-07-19 03:27:36 +0000

    Refine haikuWriterAgent instructions and handoff description for clarity and accuracy

--- src/app/agentConfigs/simpleHandoff.ts
+++ src/app/agentConfigs/simpleHandoff.ts
@@ -6,18 +6,17 @@ export const haikuWriterAgent = new RealtimeAgent({
   name: 'haikuWriter',
   voice: 'sage',
   instructions:
-    'Spurðu notenda um eitthvað mjög íslenskt , og svo gera smá brandara, alltaf tala skýra og fallega íslensku.',
+    'þú spyrð um viðfansefi og gerir svo fallegt ljóð um það, .',
   handoffs: [],
   tools: [],
-  handoffDescription: 'Þjónustufulltrúi sem getur skrifað ljóð og brandara',
+  handoffDescription: 'Þjónustufulltrúi sem getur skrifað ljóð um viðfangsefni sem notandi spyr um',
 });
 
 export const greeterAgent = new RealtimeAgent({
   name: 'greeter',
   voice: 'sage',
   instructions:
-    "Þú ert hjálpsamur nýr þjónustufulltrúi. Hlutverk þitt er að heilsa notandanum á íslensku og vísa honum á réttan sérfræðing sem getur hjálpað honum frekar.\n\n" +
-    'Þú ert mjög nýr og getur aðeins sinnt einföldum verkefnum, og treystir mikið á yfirmanninn með getNextResponseFromSupervisor verkfærið.\n\n' +
+    "Þú ert hjálpsamur nýr þjónustufulltrúi. Hlutverk þitt er að heilsa notandanum á íslensku og vísa honum á réttan sérfræðing sem getur hjálpað honum frekar.\n\n" ,
   handoffs: [haikuWriterAgent],
   tools: [],
   handoffDescription: 'Agent that greets the user',
